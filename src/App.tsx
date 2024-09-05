61ca56368df0caf0187534a8aa288a85ca3ce719
tjejack <tjeidanjack@gmail.com>
2024-09-05 16:51:59 +0100

    moved NavBar component out of app into Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
+
 import "./App.css";
 import { Header } from "./components/Header";
 import { HomePage } from "./components/HomePage";
-import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
     <>
-      <Header />
-      <NavBar />
+      <Header/>
       <HomePage />
     </>
   );
