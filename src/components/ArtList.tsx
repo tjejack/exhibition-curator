4e8f4a07b8b71e3e7aa5b23fc69c37b4a304f171
tjejack <tjeidanjack@gmail.com>
2024-09-12 18:07:05 +0100

    import utils get function and useState

--- src/components/ArtList.tsx
+++ src/components/ArtList.tsx
@@ -1,10 +1,19 @@
-import { ArtCard } from "./ArtCard"
+import { useEffect, useState } from "react";
+import { ArtCard } from "./ArtCard";
+import { getMetArtIds } from "../utils/getMetArtIds";
+
 
 export const ArtList = () => {
-    return (
-        <div>
-            <p>This is the art list.</p>
-            <ArtCard />
-        </div>
-    )
-}
+  const [metArtIDsArray, setMetArtIDsArray] = useState<number[]>([]);
+  useEffect(() => {
+    getMetArtIds().then((metArtIdData: number[]) => {
+      setMetArtIDsArray(metArtIdData);
+    });
+  }, []);
+  return (
+    <div>
+      <p>This is the art list.</p>
+      <ArtCard />
+    </div>
+  );
+};
