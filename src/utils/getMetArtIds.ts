03c3f7be38dbeda60e7cee9437128e5cf04e24ca
tjejack <tjeidanjack@gmail.com>
2024-09-14 17:59:45 +0100

    remove met utils

--- src/utils/getMetArtIds.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const getMetArtIds = () => {
-    return axios.get('https://collectionapi.metmuseum.org/public/collection/v1/objects').then(({data}) => {
-        return data;
-    })
-};
