7d6084ff2348a88882309609f74b6f2f03f7e3e7
tjejack <tjeidanjack@gmail.com>
2024-09-14 18:01:55 +0100

    add chicago art functionality

--- src/components/ArtCard.tsx
+++ src/components/ArtCard.tsx
@@ -1,7 +1,13 @@
-export const ArtCard = () => {
-    return (
-        <div>
-            <p>This is an art card.</p>
-        </div>
-    )
+import { ChicagoArtObject } from "../contexts/ChicagoArtContext";
+
+interface Props {
+  artObject: ChicagoArtObject;
 }
+
+export const ArtCard = (props: Props) => {
+  return (
+    <div>
+      <p>{props.artObject.title}</p>
+    </div>
+  );
+};
