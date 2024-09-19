244a9f155e7603f7de9d493464fe7ccc267e97eb
tjejack <tjeidanjack@gmail.com>
2024-09-19 19:07:36 +0100

    create gradient for home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'gradient-to-45': 'linear-gradient(45deg, #34d399, #93c5fd, #2dd4bf)'
+      }),
+    },
   },
   plugins: [],
 };
