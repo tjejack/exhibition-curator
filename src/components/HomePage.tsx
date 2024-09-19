e502df31c49ce6ebee1b44c40ec6834119d320b0
tjejack <tjeidanjack@gmail.com>
2024-09-19 19:09:31 +0100

    styling

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,9 +1,20 @@
 export const HomePage = () => {
-    return(
-        <div id="home-page">
-            <h2>Welcome!</h2>
-            <p>Welcome to SMART Gallery - a virtual art gallery right at your fingertips!</p>
-            <p>This project was built by Tj Jack (he/him) for the Tech Returners program.</p>
+    return (
+      <div id="homepage" className="relative isolate overflow-hidden bg-gradient-to-45 py-24 flex-1 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <h2 className="text-4xl font-bold tracking-tight text-black sm:text-6xl">Welcome!</h2>
+            <p className="mt-6 text-lg leading-8 text-black">
+            Welcome to SMART Gallery - a virtual art gallery right at your fingertips!
+            </p>
+            <p className="mt-6 text-md leading-8 text-black">
+            To get started, why not head on over to the Explore Art tab on your nav bar?
+            </p>
+            <p className="mt-6 text-sm leading-8 text-gray-800">
+            This project was built by Tj Jack (he/him) for the Tech Returners program.
+            </p>
+          </div>
         </div>
+      </div>
     )
-}
+  }
