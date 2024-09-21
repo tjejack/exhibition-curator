65dd1ceead62089c96c672ab8d8bec1e799a01f2
tjejack <tjeidanjack@gmail.com>
2024-09-21 19:23:15 +0100

    add pagination interface and searchparams

--- src/components/ArtExplore.tsx
+++ src/components/ArtExplore.tsx
@@ -1,11 +1,15 @@
 import { ArtList } from "./ArtList";
 import { Search } from "./Search";
+import { PaginationUI } from "./PaginationUI";
+import { useSearchParams } from "react-router-dom";
 
 export const ArtExplore = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
   return (
     <div>
       <Search />
       <ArtList />
+      <PaginationUI searchParams={searchParams} setSearchParams={setSearchParams}/>
     </div>
   );
 };
