6805d112b40d14909cd9b206b97c9315d455cfc4
tjejack <tjeidanjack@gmail.com>
2024-09-23 18:47:48 +0100

    add limit

--- src/utils/getChicagoArt.ts
+++ src/utils/getChicagoArt.ts
@@ -1,19 +1,24 @@
 import axios from "axios";
 import { ArtObject } from "../contexts/ArtObject";
 import { ChicagoArtObject } from "../contexts/ChicagoArtContext";
+import { SearchQueries } from "../contexts/SearchQueries";
 
 
-export const getChicagoArt = () => {
-  return axios.get(`https://api.artic.edu/api/v1/artworks?limit=10`).then(({ data }) => {
-    const dataArray: ArtObject[] | PromiseLike<ArtObject[]> = [];
-    data.data.forEach((artwork: ChicagoArtObject) => dataArray.push({
-      id: artwork.id,
-      title: artwork.title,
-      artist: artwork.artist_title,
-      image_id: artwork.image_id,
-      date: artwork.date_end,
-      api: 'Chicago',
-  }))
-    return dataArray;
-  });
+export const getChicagoArt = (props: SearchQueries) => {
+  return axios
+    .get(`https://api.artic.edu/api/v1/artworks?limit=${props.limit}&page=${props.page}`)
+    .then(({ data }) => {
+      const dataArray: ArtObject[] | PromiseLike<ArtObject[]> = [];
+      data.data.forEach((artwork: ChicagoArtObject) =>
+        dataArray.push({
+          id: artwork.id,
+          title: artwork.title,
+          artist: artwork.artist_title,
+          image: `https://www.artic.edu/iiif/2/${artwork.image_id ? artwork.image_id : "NOIMG"}/full/843,/0/default.jpg`,
+          date: artwork.date_end,
+          api: "Chicago",
+        })
+      );
+      return dataArray;
+    });
 };
