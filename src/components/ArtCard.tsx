50e463d1bb653ef8de40bb0a112db48597ddfa7e
tjejack <tjeidanjack@gmail.com>
2024-09-23 18:50:50 +0100

    temporary removal of error handling

--- src/components/ArtCard.tsx
+++ src/components/ArtCard.tsx
@@ -5,22 +5,18 @@ interface Props {
 }
 
 export const ArtCard = (props: Props) => {
-
   return (
     <div key={props.artObject.id} className="group relative">
-      {props.artObject.image_id ? (
         <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md lg:aspect-none group-hover:opacity-75 lg:h-80">
           <img
             alt={props.artObject.title}
-            src={`https://www.artic.edu/iiif/2/${props.artObject.image_id}/full/843,/0/default.jpg`}
+            src={props.artObject.image}
             className="h-full w-full object-cover object-center lg:h-full lg:w-full"
           />
         </div>
-      ) : (
-        <div className="aspect-h-1 aspect-w-1 w-full h-full overflow-hidden rounded-md bg-gray-200 inline-flex items-center justify-center lg:aspect-none group-hover:opacity-75 lg:h-80">
+        {/* <div className="aspect-h-1 aspect-w-1 w-full h-full overflow-hidden rounded-md bg-gray-200 inline-flex items-center justify-center lg:aspect-none group-hover:opacity-75 lg:h-80">
           <p>Image Unavailable</p>
-        </div>
-      )}
+        </div> */}
       <div className="mt-4 flex justify-between">
         <div>
           <h3 className="text-sm text-gray-700">
