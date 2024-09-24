35198dec33661d0708bb0157dec6f8325d823a53
tjejack <tjeidanjack@gmail.com>
2024-09-24 18:01:23 +0100

    remove sort

--- src/components/ArtList.tsx
+++ src/components/ArtList.tsx
@@ -11,11 +11,12 @@ export const ArtList = (props: Props) => {
   const [artArray, setArtArray] = useState<ArtObject[]>([]);
   const pageNumber = props.searchParams.get("page");
   const pageLimit = props.searchParams.get("limit");
+  const textQuery = props.searchParams.get("q");
   useEffect(() => {
-    getArt({page: pageNumber, limit: pageLimit}).then((artData: ArtObject[]) => {
+    getArt({page: pageNumber, limit: pageLimit, q: textQuery}).then((artData: ArtObject[]) => {
       setArtArray(artData);
     });
-  }, [pageNumber, pageLimit]);
+  }, [pageNumber, pageLimit, textQuery]);
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-6 sm:px-6 sm:py-2 lg:max-w-7xl lg:px-8">
