a2da0df1ba863e67b52dd9eb3e649225671de79c
tjejack <tjeidanjack@gmail.com>
2024-09-25 18:18:49 +0100

    styling and props padding

--- src/components/ArtExplore.tsx
+++ src/components/ArtExplore.tsx
@@ -6,10 +6,18 @@ import { useSearchParams } from "react-router-dom";
 export const ArtExplore = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   return (
-    <div>
-      <Search searchParams={searchParams} setSearchParams={setSearchParams}/>
-      <ArtList searchParams={searchParams}/>
-      <PaginationUI searchParams={searchParams} setSearchParams={setSearchParams}/>
-    </div>
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl px-6 py-5 sm:px-2 sm:py-8 lg:max-w-7xl lg:px-8">
+          <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+            Explore Art
+          </h2>
+          <Search />
+          <ArtList searchParams={searchParams} />
+          <PaginationUI
+            searchParams={searchParams}
+            setSearchParams={setSearchParams}
+          />
+        </div>
+      </div>
   );
 };
