760411f97cd074cbee3eb4caaca07be06c2005a5
tjejack <tjeidanjack@gmail.com>
2024-09-25 18:20:16 +0100

    styling

--- src/components/SearchTextQuery.tsx
+++ src/components/SearchTextQuery.tsx
@@ -13,7 +13,7 @@ export const SearchTextQuery = (props: Props) => {
   return (
     <div className="w-full max-w-md px-1">
       <Field>
-        <Label className="text-sm/6 font-medium text-black">Search</Label>
+        <Label className="text-sm/6 font-medium text-black" htmlFor="search-box">Search</Label>
         <div className="relative mt-2">
           <Input
             className="block w-full rounded-md border-0 py-2 pl-3 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500 placeholder:text-gray-400 sm:text-sm sm:leading-6"
