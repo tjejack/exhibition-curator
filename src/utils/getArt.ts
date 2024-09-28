7be3f885227276bec51f5cefc6d91edaeff7616d
tjejack <tjeidanjack@gmail.com>
2024-09-28 18:36:36 +0100

    get art for new search params

--- src/utils/getArt.ts
+++ src/utils/getArt.ts
@@ -7,21 +7,41 @@ interface Props {
   page: string | null;
   limit: string | null;
   q: string | null;
+  api: string | null;
 }
 
 export async function getArt(props: Props) {
-  const searchQueries: SearchQueries = { page: "1", limit: "10", };
+  const searchQueries: SearchQueries = { page: "1", limit: "10" };
   if (props.page !== null && !isNaN(parseInt(props.page))) {
     searchQueries.page = props.page;
   }
   if (props.limit !== null && !isNaN(parseInt(props.limit))) {
-    searchQueries.limit = `${Math.ceil(parseInt(props.limit) / 2)}`;
+    if(props.api !== "Chicago" && props.api !== "Chicago"){
+      searchQueries.limit = `${Math.ceil(parseInt(props.limit) / 2)}`;
+    } else {
+      searchQueries.limit = props.limit;
+    }
   }
-  if(props.q !== null){
+  if (props.q !== null) {
     searchQueries.q = props.q;
   }
-  const chicagoArt = await getChicagoArt(searchQueries);
-  const clevelandArt = await getClevelandArt(searchQueries);
-  const artArray: ArtObject[] = [...chicagoArt, ...clevelandArt];
-  return artArray;
+  try {
+    const artArray: ArtObject[] = [];
+    if (props.api === "Cleveland") {
+      searchQueries.API = "cleveland";
+      const clevelandArt = await getClevelandArt(searchQueries);
+      artArray.push(...clevelandArt);
+    } else if (props.api === "Chicago") {
+      searchQueries.API = "chicago";
+      const chicagoArt = await getChicagoArt(searchQueries);
+      artArray.push(...chicagoArt);
+    } else {
+      const chicagoArt = await getChicagoArt(searchQueries);
+      const clevelandArt = await getClevelandArt(searchQueries);
+      artArray.push(...clevelandArt, ...chicagoArt);
+    }
+    return artArray;
+  } catch {
+    return [];
+  }
 }
