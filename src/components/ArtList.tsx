336fe08d1910a485251e849742d2b07af18c1cab
tjejack <tjeidanjack@gmail.com>
2024-09-30 18:23:44 +0100

    error response for no artwork found

--- src/components/ArtList.tsx
+++ src/components/ArtList.tsx
@@ -12,6 +12,8 @@ interface Props {
 export const ArtList = (props: Props) => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isError, setIsError] = useState<boolean>(false);
+  const [isNoArt, setIsNoArt] = useState<boolean>(false);
+  const [isNoExhibition, setIsNoExhibition] = useState<boolean>(false);
   const [tempExhibition, setTempExhibition] = useState<ArtObject[]>(
     getTempExhibition()
   );
@@ -25,12 +27,12 @@ export const ArtList = (props: Props) => {
     }
     return exhibition;
   }
-  
+
   let pageNumber: string | null = "";
   let pageLimit: string | null = "";
   let textQuery: string | null = "";
-  let apiFilter : string | null = "";
-  
+  let apiFilter: string | null = "";
+
   if (props.searchParams) {
     pageNumber = props.searchParams.get("page");
     pageLimit = props.searchParams.get("limit");
@@ -39,12 +41,23 @@ export const ArtList = (props: Props) => {
   }
 
   useEffect(() => {
+    setIsError(false);
+    setIsNoArt(false);
     setIsLoading(true);
     if (props.searchParams) {
-      getArt({ page: pageNumber, limit: pageLimit, q: textQuery, api: apiFilter })
+      getArt({
+        page: pageNumber,
+        limit: pageLimit,
+        q: textQuery,
+        api: apiFilter,
+      })
         .then((artData: ArtObject[]) => {
           setIsLoading(false);
-          setArtArray(artData);
+          if (artData.length === 0) {
+            setIsNoArt(true);
+          } else {
+            setArtArray(artData);
+          }
         })
         .catch((error) => {
           setIsLoading(false);
@@ -53,15 +66,41 @@ export const ArtList = (props: Props) => {
         });
     } else {
       setIsLoading(false);
+      if(artArray.length === 0){
+        setIsNoExhibition(true);
+      }
     }
-    
-  }, [pageNumber, pageLimit, textQuery, props.searchParams, apiFilter]);
-  
+  }, [pageNumber, pageLimit, textQuery, props.searchParams, apiFilter, artArray.length]);
+
   if (isLoading) {
     return <Loading />;
   }
   if (isError) {
-    return <Error />
+    return <Error />;
+  }
+  if (isNoArt) {
+    return (
+      <div className="py-3 text-center text-xl">
+        <p>We appear to be drawing a blank.</p>
+        <p>Perhaps your search term was too specific?</p>
+      </div>
+    );
+  }
+  if (isNoExhibition) {
+    return (
+      <div className="text-center text-xl">
+        <div className="pb-5">
+        <p>It seems your exhibition is empty!</p>
+        <p>Why not check out the Explore Art page and add some works?</p>
+        </div>
+        <a
+          href="/art"
+          className="rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+        >
+          Explore Art
+        </a>
+      </div>
+    );
   }
   return (
     <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 pb-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
